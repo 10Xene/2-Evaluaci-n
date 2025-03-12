280da7e1aa0e43a3fe5daa9bcacd32dfbdd7ec79
Usuario <CORREO>
2025-03-12 19:55:54 +0100

    Python

---
+++ Examen 2º Evaluación.py
@@ -0,0 +1,23 @@
+def buscarPalabra(objetivo, palabras):
+# Crear lista de nombres.
+nombres= Perantano, Zutano, Fulano, Mengano
+edades= 0, 25, 50, 75
+
+def imprimirListaInversa(lista):
+
+
+nombres = ["Mengano", "Fulano", "Zutano", "Perantano"]
+edades = {
+    "Mengano": 0,
+    "Fulano": 25,
+    "Zutano": 50,
+    "Perantano": 75
+}
+ for nombre in nombres:
+        # Si la letra está en vocales
+        if nombre in nombres:
+            # Es una vocal, se suma 1 al diccionario por clave
+            resultado[nombre]
+   
+    # Devolver el resultado
+    return nombres
